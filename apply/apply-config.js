1ab393111734580c791c9d4b3f2c3ee227fbbf42
William Keffer <williamkeffer@Williams-MacBook-Pro-4.local>
2026-07-03 16:10:54 -0700

    Configure application endpoint

--- apply/apply-config.js
+++ apply/apply-config.js
@@ -1,1 +1,1 @@
-window.NSCC_APPLY_ENDPOINT = 'PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE';
+window.NSCC_APPLY_ENDPOINT = 'https://script.google.com/macros/s/AKfycbwzj4_5PHd0GKZWYxocgZGpeTEd-bxz9zUXS0E5OJ025NYix4dUcwr3MEQAbK_Wu6U/exec';
